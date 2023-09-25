eea1d2c22b3662dd8e8de9dfbba02432781dc1c1
thienha1510 <thienha151000@gmail.com>
2023-09-25 16:51:33 +0700

    new update

--- js/script.js
+++ js/script.js
@@ -13,6 +13,6 @@ window.onscroll = () =>{
    if(window.scrollY > 0){
       document.querySelector('.header').classList.add('active');
    }else{
-      document.querySelector('.header').classList.remove('active');
+      document.querySelector('.header  ').classList.remove('active');
    }
 }
